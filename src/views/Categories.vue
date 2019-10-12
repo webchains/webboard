5f14d02bebfa297ba97a7752e515d919a28d7719
zedsa <37799569+zedsa@users.noreply.github.com>
2019-10-12 00:36:31 -0500

    add files

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -3,16 +3,19 @@
         <b-row><b-col><p>Results: <b-button @click="limit = 10" class="mx-1">10</b-button><b-button @click="limit = 25" class="mx-1">25</b-button><b-button @click="limit = 50" class="mx-1">50</b-button><b-button @click="limit = 75" class="mx-1">75</b-button><b-button @click="limit = 100" class="mx-1">100</b-button></p></b-col></b-row>
         <b-row>
             <b-col v-if="categories.popular && categories.popular.docs.length">
+                <p>Popular</p>
                 <Category v-for="category of categories.popular.docs" :key="category._id" :category="category"/>
                 <b-pagination v-model="popularPage" :total-rows="categories.popular.total" :per-page="limit" align="fill"></b-pagination>
             </b-col>
             <b-col v-else><p>no categories yet</p></b-col>
             <b-col v-if="categories.new && categories.new.docs.length">
+                <p>New</p>
                 <Category v-for="category of categories.new.docs" :key="category._id" :category="category"/>
                 <b-pagination v-model="newPage" :total-rows="categories.new.total" :per-page="limit" align="fill"></b-pagination>
             </b-col>
             <b-col v-else><p>no categories yet</p></b-col>
             <b-col v-if="categories.updated && categories.updated.docs.length">
+                <p>Updated</p>
                 <Category v-for="category of categories.updated.docs" :key="category._id" :category="category"/>
                 <b-pagination v-model="updatedPage" :total-rows="categories.updated.total" :per-page="limit" align="fill"></b-pagination>
             </b-col>
